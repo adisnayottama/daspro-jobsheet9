4caf58f2c29e71e7c04288d2e43c09d8863b722c
adisnayottama <adisnayottama1@gmail.com>
2025-11-06 07:53:59 -0800

    praktik2

--- ArrayNilai02.java
+++ ArrayNilai02.java
@@ -9,6 +9,7 @@ public class ArrayNilai02 {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt ();
         } for (int i = 0; i < 10; i++) {
+            
             System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
         }
     }

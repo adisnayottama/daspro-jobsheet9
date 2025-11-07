a680d477d10bb826c138e060f153f0dc3e0a69ac
adisnayottama <adisnayottama1@gmail.com>
2025-11-06 20:00:52 -0800

    modifpraktik4

--- SearchNilai02.java
+++ SearchNilai02.java
@@ -1,17 +1,34 @@
+import java.util.Scanner;
+
 public class SearchNilai02 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan banyak nilai yang akan diinput: ");
+        int jml = sc.nextInt();
+        int[] arrNilai = new int[jml];
 
         for (int i = 0; i < arrNilai.length; i++) {
-            if  (key == arrNilai[i]) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        int hasil = -1;
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (key == arrNilai[i]) {
                 hasil = i;
                 break;
             }
         }
+
         System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
-        System.out.println();
+
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan dalam data.");
+        }
+
     }
 }

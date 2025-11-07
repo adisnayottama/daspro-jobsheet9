a9f385054b37b4987262e58f52460be70afd799c
adisnayottama <adisnayottama1@gmail.com>
2025-11-06 19:44:42 -0800

    modifpraktik3

--- ArrayRataNilai02.java
+++ ArrayRataNilai02.java
@@ -2,17 +2,32 @@ import java.util.Scanner;
 public class ArrayRataNilai02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs= new int[10];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMhs = sc.nextInt();
+
+        int[] nilaiMhs= new int[jmlMhs];
+        int jmlLulus = 0, jmlTdkLulus = 0;
         double total = 0;
         double rata2;
+        double totLulus = 0, totTdkLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
-        } for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+
+            if (nilaiMhs[i] > 70) {
+                totLulus += nilaiMhs[i];
+                jmlLulus++;
+            } else {
+                totTdkLulus += nilaiMhs[i];
+                jmlTdkLulus++;
+            }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+        
+        double rataLulus = totLulus / jmlLulus;
+        double rataTidakLulus = totTdkLulus / jmlTdkLulus;
+
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
     }
 }
